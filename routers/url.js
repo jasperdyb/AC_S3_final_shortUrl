f004d1f6e670e2a7bd4ec5ffe821689b1f12ff46
Jasper Chen <jasperdyb@gmail.com>
2020-04-08 13:42:24 +0800

    Add key generation function

--- routers/url.js
+++ routers/url.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const Urls = require('../models/url')
 
+const hashLength = 5
+
 function validURL(str) {
   var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
     '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
@@ -12,12 +14,23 @@ function validURL(str) {
   return !!pattern.test(str);
 }
 
+function generateRandomKey() {
+  const characters = "0123456789ABCDEFGHIJKLMNPQRSTUVWXYZ";
+  let string = "";
+  for (let i = 0; i < hashLength; i++) {
+    string += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return string;
+}
+
 router.post('/', (req, res, next) => {
 
   console.log(validURL(req.body.url))
 
   if (validURL(req.body.url)) {
+    let key = generateRandomKey()
     console.log("This is a Url")
+    console.log(`key = ${key}`)
   }
   else {
     console.log("This is not a Url")
